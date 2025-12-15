fdd99df8a7d97ab503b8462f8074730a0b547bc1
hauvoong <hauvoong@gmail.com>
2025-12-15 11:52:05 +0000

    Add allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^+68i8ew3reta=2r^)61@!5$pp4016b$=wpbat52s8gtz3jrtl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
