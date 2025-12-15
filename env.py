59188c25469f89ae1162b38ee35e77ed172f4faa
hauvoong <hauvoong@gmail.com>
2025-12-15 12:08:40 +0000

    Add database

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_jTEuzqGson96@ep-lingering-star-agelo7as.c-2.eu-central-1.aws.neon.tech/salsa_risk_nutty_382682")
