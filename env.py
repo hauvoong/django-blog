7cd477013eb369e4170f2764b09d4817bc0ba60f
hauvoong <hauvoong@gmail.com>
2025-12-31 14:51:05 +0000

    Add Cloudinary

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_jTEuzqGson96@ep-lingering-star-agelo7as.c-2.eu-central-1.aws.neon.tech/salsa_risk_nutty_382682")
 os.environ.setdefault("SECRET_KEY", "django-insecure-^+68i8ew3reta=2r^)61@!5$pp4016b$=wpbat52s8gtz3jrtl")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://144545911698871:OAn0Qs6M7D6tGXXjY8X6tdN2Ul8@ddyrc8irg")
